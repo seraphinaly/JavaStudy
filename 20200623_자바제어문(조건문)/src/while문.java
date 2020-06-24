e5aacb35e0eedfc0b1466dd99acf2daea8b38b78
SIST <SIST@DESKTOP-1RQ1LS6>
2020-06-24 10:09:22 +0900

    자바제어문

--- 20200623_자바제어문(조건문)/src/while문.java
+++ 20200623_자바제어문(조건문)/src/while문.java
@@ -2,15 +2,15 @@
 public class while문 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int treeHit = 0;
-		while (treeHit < 10) {
-		    treeHit++;
-		    System.out.println("나무를  " + treeHit + "번 찍었습니다.");
-		    if (treeHit == 10) {
-		        System.out.println("나무 넘어갑니다.");
+	
+	int treeHit=0;
+	while(treeHit<10) {
+		treeHit++;
+		System.out.println("나무를 "+treeHit+"번 찍었습니다.");
+	    }
+		if(treeHit==10) {
+			System.out.println("나무 넘어갑니다.");
+		
 		    }
 		}
 	}
-
-}
